38329fb4bfe8cb9ca71a943e4d6d0099c18f10e3
Amanjot Girn <asgirn520@gmail.com>
2026-04-21 18:57:40 -0400

    Add files via upload

---
+++ InlinedPlaneSimulation.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+import java.awt.Color;
+
+public class InlinedPlaneSimulation {
+    public static void main(String args[]) {
+        JFrame frame = new JFrame("Inclined Plane Simulation");
+        frame.setVisible(true);
+        frame.setSize(700, 450);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
